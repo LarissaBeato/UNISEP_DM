5339ca09e9643a9dbd762fec19fa1c0ceef1460c
Larissa Beato <larissabonkoskibeato@gmail.com>
2026-09-01 20:08:21 -0300

    01/09

--- primeira_api/index.js
+++ primeira_api/index.js
@@ -1,40 +1,75 @@
-const express = require('express');
+const express = require("express");
+
 const app = new express();
 
 app.use(express.json());
 
-var data = [{
-    nome: "Larissa",
-    cpf: "123.456.789-00",
-    status: true
-}];
+var contador_id = 1;
 
-app.get('/Listar', (request, response) => {
+var data = [
+    {
+        id: 1,
+        nome: "Larissa",
+        cpf: "99999999999",
+        status: true
+    }
+];
+
+
+app.get("/listar", (request, response) => {
     return response.send(data);
 });
 
-app.post("/cadastrar", (request, response) => {
-    const { nome, cpf, status } = request.body;
-/*     const nome = request.body.nome;
-    const cpf = request.body.cpf;
-    const status = request.body.status; */
-    
-/*     console.log("Dados da pessoa:");
-    console.log(nome);
-    console.log(cpf);
-    console.log(status); */
 
-    data.push({
-        nome: nome,
-        cpf: cpf,
-        status: status
+app.get("/listar/:id", (request, response) => {
+
+    const { id } = request.params;
+
+    const pessoa = data.filter((item) => {
+        return item.id == id;
     });
 
-    return response.send("Pessoa cadastrada com sucesso!");
+    if (pessoa.length == 0) {
+        response.status(400).send({
+            msg: "Pessoa do código " + id + " não encontrada!"
+        });
+    }
+
+    response.send(pessoa);
 });
+
+
+app.post("/cadastrar", (request, response) => {
+
+    const { nome, cpf, status } = request.body;
+
+    if (!cpf) {
+        return response.status(300).send({
+            msg: "O campo CPF é obrigatório!"
+        });
+    }
+
+    contador_id++;
+
+    data.push({
+        id: contador_id,
+        nome,
+        cpf,
+        status
+    });
+
+    response.status(200).send({
+        msg: "Pessoa cadastrada com sucesso!"
+    });
+});
+
 
 app.listen(8080, () => {
-    console.log('Servidor rodando na porta 8080');
-
-    console.log('teste');
+    console.log("O servidor está rodando na porta 8080!");
 });
+
+
+// Status 500 -- erro não catalogado (interno)
+// Status 200 -- sucesso
+// Status 400 -- não conseguiu encontrar informações
+// Status 300 -- validações
