75fccfe7ca60f5cf4da4d54af17ffaae1146a8da
Larissa Beato <larissabonkoskibeato@gmail.com>
2026-09-01 22:03:48 -0300

    01/09 - 2

--- primeira_api/index.js
+++ primeira_api/index.js
@@ -15,14 +15,11 @@ var data = [
     }
 ];
 
-
 app.get("/listar", (request, response) => {
     return response.send(data);
 });
 
-
 app.get("/listar/:id", (request, response) => {
-
     const { id } = request.params;
 
     const pessoa = data.filter((item) => {
@@ -30,23 +27,21 @@ app.get("/listar/:id", (request, response) => {
     });
 
     if (pessoa.length == 0) {
-        response.status(400).send({
+        return response.status(400).send({
             msg: "Pessoa do código " + id + " não encontrada!"
         });
     }
 
-    response.send(pessoa);
+    return response.send(pessoa);
 });
 
-
 app.post("/cadastrar", (request, response) => {
-
     const { nome, cpf, status } = request.body;
 
-    if (!cpf) {
-        return response.status(300).send({
-            msg: "O campo CPF é obrigatório!"
-        });
+    if (!nome) {
+        return response.status(300).send("O campo Nome é obrigatório!");
+    } else if (!cpf) {
+        return response.status(300).send("O campo CPF é obrigatório!");
     }
 
     contador_id++;
@@ -58,18 +53,51 @@ app.post("/cadastrar", (request, response) => {
         status
     });
 
-    response.status(200).send({
+    return response.status(200).send({
         msg: "Pessoa cadastrada com sucesso!"
     });
 });
 
+app.delete("/deletar/:id", (request, response) => {
+    const { id } = request.params;
+
+    const indice = data.findIndex((item) => {
+        return item.id == id;
+    });
+
+    data.splice(indice, 1);
+
+    response.send(data);
+});
+
+app.put("/atualizar", (request, response) => {
+    const {id , nome, cpf, status} = request.body;
+
+    const indicePessoa = data.findIndex((item) => {
+        return item.id == id;
+    });
+
+    if(indicePessoa == -1){
+
+        response.status(400).send({msg: "O id " + id + " não existe!"});
+
+    }else{
+
+    data[indicePessoa].nome = nome;
+    data[indicePessoa].cpf = cpf;
+    data[indicePessoa].status = status;
+
+    }
+
+    
+});
 
 app.listen(8080, () => {
     console.log("O servidor está rodando na porta 8080!");
 });
 
-
 // Status 500 -- erro não catalogado (interno)
 // Status 200 -- sucesso
 // Status 400 -- não conseguiu encontrar informações
 // Status 300 -- validações
+
